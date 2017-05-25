2f89fcbf010335db95727bf95672ecf0ff140bcf
david <areq22@aliyun.com>
2017-05-25 21:45:41 +0800

    improve image url for aliyun oss

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -10,6 +10,5 @@ end
 
 Paperclip.interpolates :aliyun_style do |attachment, style_name|
   #需要阿里云设置相应服务样式
-  style_name||='default'
-  '?x-oss-process=style/'+style_name
+  '/'+(style_name||'default'）
 end
