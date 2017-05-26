bad8356315fee351e803dfc5e3e9f0d6f0cb4d87
david <areq22@aliyun.com>
2017-05-26 14:40:24 +0800

    improve qrscene_20170530

--- app/controllers/api/v1/wechats_controller.rb
+++ app/controllers/api/v1/wechats_controller.rb
@@ -15,7 +15,8 @@ class Api::V1::WechatsController < ApplicationController
   # https://mp.weixin.qq.com/wiki?id=mp1443433542 生成带参数的二维码
   # 公众号收到未关注用户扫描qrscene_xxxxxx二维码时。注意此次扫描事件将不再引发上条的用户加关注事件
   on :scan, with: 'qrscene_20170530' do |request, ticket|
-    request.reply.text "Unsubscribe user #{request[:FromUserName]} Ticket #{ticket} http://wx-wcpc-staging.getstore.cn"
+    Rails.logger.debug "Unsubscribe user #{request[:FromUserName]} Ticket #{ticket}"
+    request.reply.text "谢谢关注，欢迎,请点击 <a href='http://wx-wcpc-staging.getstore.cn'>参加活动 </a>"
   end
 
 end
