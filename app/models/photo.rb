bad53d14a8b333ea3dbb92cc017483f9a1173073
david <areq22@aliyun.com>
2017-05-26 19:28:50 +0800

    修正上传按钮看不到问题,添加图片样式big,加数图片加载

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/pic_160.png"
+  has_attached_file :photo, styles: { big: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/pic_160.png"
   include PaperclipAliyunOssHelper
 
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
