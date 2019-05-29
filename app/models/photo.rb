e2f10579bfb83285e003d4ec9282945d214aba1b
qinghe <david@getstore.cn>
2019-05-30 00:59:14 +0800

    fix photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,20 @@ class Photo < ApplicationRecord
 
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
+
+  #https://stackoverflow.com/questions/27234065/how-to-upload-a-base-64-image-to-rails-paperclip
+  attr_accessor :base64_image
+
+  before_validation :save_base64_image
+
+  # call this explicitly from the controller or in an after_save callback
+  # after setting the base64_image attribute
+  def save_base64_image
+   if base64_image.present?
+       adapter = Paperclip.io_adapters.for(base64_image)
+       adapter.original_filename = "base64.jpg"
+       self.photo = adapter
+     end
+  end
+  
 end
--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -18,20 +18,7 @@ class Photograph < ApplicationRecord
 
   cattr_accessor :upload_limit
 
-  #https://stackoverflow.com/questions/27234065/how-to-upload-a-base-64-image-to-rails-paperclip
-  attr_accessor :base64_image
 
-  before_validation :save_base64_image
-
-  # call this explicitly from the controller or in an after_save callback
-  # after setting the base64_image attribute
-  def save_base64_image
-   if base64_image.present?
-       adapter = Paperclip.io_adapters.for(base64_image)
-       adapter.original_filename = "base64.jpg"
-       self.photo = adapter
-     end
-  end
 
   self.per_page = 6
   self.upload_limit = 1
