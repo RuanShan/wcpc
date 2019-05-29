86578010f2ea4f97ae8df7ec2713d664fc2b17d8
qinghe <david@getstore.cn>
2019-05-30 01:24:13 +0800

    Update photographs_controller.rb

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -81,7 +81,7 @@ class PhotographsController < ApplicationController
     end
     if params["photograph"]["photos_attributes"].present?
       photo_params = params["photograph"]["photos_attributes"]['0']
-      #Rails.logger.debug  photo_params.inspect
+      Rails.logger.debug  photo_params.inspect
       photo = Photo.find( photo_params['id'].to_i )
       photo.update( photo_params )
     end
