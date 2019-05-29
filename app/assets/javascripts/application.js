b07560ebed6f4949fa6d0b1fc324fe73a4682a5a
qinghe <david@getstore.cn>
2019-05-30 01:56:17 +0800

    fuck2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -121,10 +121,10 @@ $(function() {
 });
 
 // 用户提交
-//$(document).on('click', '#submitPhotoBtn', function(){
-//  // 不需要发送file， use base64
-//  $('.weui-uploader__input').attr("disabled",true);
-//})
+$(document).on('click', '#submitPhotoBtn', function(){
+  // 不需要发送file， use base64
+  $('.weui-uploader__input').attr("disabled",true);
+})
 //title: "活动介绍",
 //text: <%== ( render :partial => 'intro',:formats=>[:html] ).to_json %>,
 //buttons: [{ text: "关闭", onClick: function(){ $.closeModal();} } ]
@@ -254,7 +254,7 @@ function preview_photo(input, index) {
               var base64 = canvas.toDataURL('jpg', 0.8);
               img.src = base64;
               //输出转换后的流
-              //$('.base64_image').val( base64 );
+              $('.base64_image').val( base64 );
 
             }
           }
